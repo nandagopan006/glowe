dda4b32dde2ae26e0c64c60e5f29f4b8248971e2
Nandagopan <nandhukr006@gmail.com>
2026-03-10 10:49:56 +0530

    first commit

---
+++ glowe/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def signup_page(request):
+    
+    
+   
+    
+    return render(request, "signup.html")
+
+
+# Create your views here.
