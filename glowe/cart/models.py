826f5244319f9a9ee10342f9f4cb70e0cbfb78d5
Nandagopan <nandhukr006@gmail.com>
2026-03-30 19:37:06 +0530

    added cart app and model added cart and CartItem

--- glowe/cart/models.py
+++ glowe/cart/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from product.models import Variant,Product
 
-# Create your models here.
+
+class Cart(models.Model):
+    user=models.ForeignKey(
+        settings.AUTH_USER_MODEL,  
+        on_delete=models.CASCADE,
+        related_name='carts'
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    variant=models.ForeignKey(Variant, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
--- glowe/cart/views.py
+++ glowe/cart/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from product.models import Variant,Product
+
+# def cart(request):
+    
+#     return render(request,'cart.html')
+    
+
+
