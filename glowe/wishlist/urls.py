b70ffbf7982feb2ea1c8498c0a5215e0ba9352ec
Nandagopan <nandhukr006@gmail.com>
2026-04-03 14:34:07 +0530

    feat(wishlist): add add-to-cart functionality in views

--- glowe/wishlist/urls.py
+++ glowe/wishlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("wishlist/", views.wishlist_page, name="wishlist"),
+    path("wishlist/add/<int:variant_id>/", views.add_to_wishlist, name="add_to_wishlist"),
 ]
--- glowe/wishlist/views.py
+++ glowe/wishlist/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .models import Wishlist
+from product.models import Variant
+
+
+def add_to_wishlist(request, variant_id):
+    variant = get_object_or_404(Variant, id=variant_id, is_active=True)
+
+    if Wishlist.objects.filter(user=request.user, variant=variant).exist():
+        messages.warning(request, "Already in wishlist")
+
+    else:
+        Wishlist.objects.create(user=request.user, variant=variant)
+        messages.success(request, "Added to wishlist")
+
+    wishlist_items = Wishlist.objects.filter(user=request.user).select_related(
+        "variant", "variant__product"
+    )
+
+    return render(request, "wishlist/wishlist.html",{"wishlist_items": wishlist_items})
+
+def wishlist_page(request):
+    return render(request, "wishlist/wishlist.html")
