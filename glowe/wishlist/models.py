100aaaa8b4a9a7491267873c345fade937f38df0
Nandagopan <nandhukr006@gmail.com>
2026-04-03 12:55:06 +0530

    feat(wishlist): create wishlist model with fields

--- glowe/wishlist/models.py
+++ glowe/wishlist/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from accounts.models import ProfileUser  
+from product.models import Variant 
 
-# Create your models here.
+class Wishlist(models.Model):
+    user=models.ForeignKey(
+        ProfileUser,on_delete=models.CASCADE,
+        related_name='wishlist_items'
+    )
+    
+    variant=models.ForeignKey(
+        Variant, on_delete=models.CASCADE, related_name="wishlisted_by"
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)   
+    
+    class Meta:
+        unique_together =('user','variant')
+        
+    def __str__(self):
+        return f"{self.user.username} - {self.variant.sku}"
+        
