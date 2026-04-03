1faf69b01261e0658cdd79919ee7c06f4672af39
Nandagopan <nandhukr006@gmail.com>
2026-04-03 21:28:57 +0530

    feat(wishlist): add move-to-cart functionality for wishlist items

--- glowe/wishlist/views.py
+++ glowe/wishlist/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 
 from .models import Wishlist
 from product.models import Variant
+from cart.models import Cart,CartItem
+from cart.utils import get_user_cart
 
 @login_required
 def toggle_wishlist(request, variant_id):
@@ -73,10 +75,50 @@ def wishlist_page(request):
         'wishlist_count':wishlist_count,
         'recommend_products':recommend_products,
     })
-
+@login_required
 def clear_wishlist(request) :
     
     if request.method=="POST":
         Wishlist.objects.filter(user=request.user).delete()
     
     return redirect("wishlist")
+
+@login_required
+def wishlist_item_to_cart(request,variant_id):
+    
+    variant =get_object_or_404(Variant,id=variant_id,is_active=True)
+    
+    # just checking product active false or delete ayo inn nokkuumm 
+    if not variant.product.is_active or variant.product.is_deleted:
+        messages.error(request,"Product not available")
+        return redirect('wishlist')
+    if variant.stock == 0:
+        messages.warning(request,"Out of stock")
+        return redirect('wishlist')
+    
+    cart = get_user_cart(request.user)
+    
+    #check if item already in cart
+    cart_item=CartItem.objects.filter(
+        cart=cart,variant=variant
+    ).first()
+    
+    if cart_item:#if already increase the qty 
+        cart_item.quantity+=1
+        cart_item.save()
+    else:# to crerate new item
+        CartItem.objects.create(cart=cart,variant=variant,quantity=1)
+    
+    Wishlist.objects.filter(user=request.user,variant=variant).delete() # pinne dlte karo hahha
+    
+    messages.success(request,"Added to cart")
+    return redirect('wishlist')
+    
+    
+        
+    
+    
+    
+    
+
+
