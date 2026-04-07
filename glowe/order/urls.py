036785847e792db5b95a262c6f23173ceebf491f
Nandagopan <nandhukr006@gmail.com>
2026-04-07 10:27:45 +0530

    feat(order) : added the url for order_detial ,order cancel and item cancel order impenttedd

--- glowe/order/urls.py
+++ glowe/order/urls.py
@@ -6,8 +6,8 @@ path('place-order/',views.place_order,name='place_order'),
 path('order-success/<int:order_id>/',views.order_success,name='order_success'),
 
 path('orders/', views.order_listing, name='order_listing'),
-path('order-detial/<int:order_id>/',views.order_detial,name='order_detial'),
-path('order-detial/<int:order_id>/cancel/',views.cancel_order,name='cancel_order'),
-path('order-detial/item/<int:item_id>/cancel/', views.cancel_order_item, name='cancel_order_item'),
+path('order-detail/<int:order_id>/',views.order_detial,name='order_detial'),
+path('order-detail/<int:order_id>/cancel/',views.cancel_order,name='cancel_order'),
+path('order-detail/item/<int:item_id>/cancel/', views.cancel_order_item, name='cancel_order_item'),
 
 ]
