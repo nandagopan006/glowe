62d401be8485d4d4e674212c04843bbb511b399a
Nandagopan <nandhukr006@gmail.com>
2026-04-21 10:16:11 +0530

    feat(wallet): add wallet views

--- glowe/wallet/views.py
+++ glowe/wallet/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Wallet
 
-# Create your views here.
+
+def wallet_view(request):
+    wallet,created = Wallet.objects.get_or_create(user=request.user)
+
+    transactions_list = wallet.transactions.all().order_by('-created_at')
+
+    paginator=Paginator(transactions_list,5)  # 5 per page
+    page_number=request.GET.get('page')
+    transactions =paginator.get_page(page_number)
+
+    return render(request,'wallet/wallet.html',{
+        'wallet':wallet,
+        'transactions':transactions
+    })
